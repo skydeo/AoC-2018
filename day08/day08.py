a55bbb456b44cb3130b68aac68933c9f96fffe9a
Alexander Stone <enderu@gmail.com>
2018-12-10 16:20:53 -0600

    day 8 part 1. had to look for a bit of help on this one.

---
+++ day08/day08.py
@@ -0,0 +1,27 @@
+import timeit
+
+test = False
+
+input_file = '08.in' if not test else '08_test.in'
+
+with open(input_file, 'r') as f:
+  puzzle_input = list(map(int, f.read().split()))
+
+
+def sum_metadata(nodes):
+  checksum = 0
+
+  child_nodes = nodes.pop(0)
+  metadata_entries = nodes.pop(0)
+
+  if child_nodes == 0:
+    checksum += sum(nodes.pop(0) for _ in range(metadata_entries))
+  else:
+    checksum += sum(sum_metadata(nodes) for _ in range(child_nodes)) + sum(nodes.pop(0) for _ in range(metadata_entries))
+  
+  return checksum
+
+
+checksum = sum_metadata(puzzle_input)
+
+print(checksum)
