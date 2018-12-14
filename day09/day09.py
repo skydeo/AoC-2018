23d49b52ab1d0370a20e1d9431ef4289457298b9
Alexander Stone <enderu@gmail.com>
2018-12-14 15:17:26 -0600

    deque. how could I forget about deque? I tried to use it earlier...

--- day09/day09.py
+++ day09/day09.py
@@ -39,9 +39,33 @@ def play_game(num_players, last_marble):
 
   return(scores)
 
+def play_game_faster(num_players, last_marble):
+  print(f"Playing a game\n{'-'*(13+len(str(last_marble)))}\nPlayers:     {num_players}\nLast marble: {last_marble}\n")
+
+  scores = defaultdict(int)
+  circle = deque([0])
+  c_marble = 0
+
+  for x in range(1,last_marble+1):
+
+    if (x % 23) != 0:
+      circle.rotate(-1)
+      circle.append(x)
+    else:
+      circle.rotate(7)
+      c_player = (x % num_players) or num_players
+      scores[c_player] += x + circle.pop()
+      circle.rotate(-1)
+
+  return(scores)
+
 
 start_time = timeit.default_timer()
 part_a = play_game(players, points)
 run_time = round(timeit.default_timer()-start_time,2)
-print(f"Player {max(part_a, key=part_a.get)} won with a max score of {max(part_a.values())}.\t({run_time}s)")
+print(f"Player {max(part_a, key=part_a.get)} won with a max score of {max(part_a.values())}.\t({run_time}s)\n")
 
+start_time = timeit.default_timer()
+part_b = play_game_faster(players, points * 100)
+run_time = round(timeit.default_timer()-start_time,2)
+print(f"Player {max(part_b, key=part_b.get)} won with a max score of {max(part_b.values())}.\t({run_time}s)\n")
